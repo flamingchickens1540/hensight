ee06d3c66370dd61c5a45c1b7db3dd3e4b829837
YayBlaze <blazeannison@icloud.com>
2024-01-10 16:05:13 -0800

    feat: made work

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Goodbye, World!</p>" 
+
+if __name__ == '__main__':
+    app.run(host='0.0.0',port=5001,debug=True)
