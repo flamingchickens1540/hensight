e0785dd8955eca0e029613006a5041acd9dd5e89
CharlieWRose <charliewrose@gmail.com>
2024-02-06 15:42:23 -0800

    feat: removal of subplots

--- main.py
+++ main.py
@@ -60,29 +60,9 @@ values = [value for value in list(descending_autoamps.values())]
 print(keys)
 print(values)
 
-fig = make_subplots(rows=2, cols=1)
-
-fig.add_trace(go.Bar(x=keys, y=values), row=1, col=1)
-fig.add_trace(go.Bar(x=values, y=keys), row=2, col=1)
-
-fig.update_layout(
-    autosize=False,
-    width=1000,
-    height=1000,
-    margin=dict(
-        l=50,
-        r=50,
-        b=100,
-        t=100,
-        pad=4
-    ),
-    paper_bgcolor="LightSteelBlue",
-)
+fig = go.Figure(go.Bar(x=keys, y=values))
 
 fig.show()
-
-
-
 
 # # Execute a query
 # postgreSQL_select_Query = 'SELECT team_key, auto_amp_succeed FROM "TeamMatches"'
