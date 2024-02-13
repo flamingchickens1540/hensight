1cfd64ac7e1dd0acc5a5c5a48f6e740a78e786cd
awwsomepotato <153149335+awwsomepotato@users.noreply.github.com>
2024-02-13 15:42:33 -0800

    fix: make the tab width consistent (4 spaces)

--- charts.py
+++ charts.py
@@ -7,57 +7,57 @@ import operator
 def convert_to_html():
 
 
-   # Define the connection parameters
-   host = "127.0.0.1"
-   port = 5432
-   database = "crescendo_scouting"
-   user = "chargedup_scouting_analysis"
-   password = "amptrapspeaker1540"
+    # Define the connection parameters
+    host = "127.0.0.1"
+    port = 5432
+    database = "crescendo_scouting"
+    user = "chargedup_scouting_analysis"
+    password = "amptrapspeaker1540"
 
 
-   # Connect to the database
-   conn = psycopg2.connect(
+    # Connect to the database
+    conn = psycopg2.connect(
        host=host,
        port=port,
        database=database,
        user=user,
        password=password,
-   )
+    )
 
 
-   # Create a cursor
-   cur = conn.cursor()
+    # Create a cursor
+    cur = conn.cursor()
 
 
-   postgresSQL_team_Query = 'SELECT team_key FROM "Teams"'
-   cur.execute(postgresSQL_team_Query)
+    postgresSQL_team_Query = 'SELECT team_key FROM "Teams"'
+    cur.execute(postgresSQL_team_Query)
 
 
-   teams = cur.fetchall()
+    teams = cur.fetchall()
 
 
-   autoamps = {}
+    autoamps = {}
 
 
-   for team in teams:
-       postgresSQL_autoamp_Query = f"""SELECT auto_amp_succeed FROM "TeamMatches" WHERE team_key='{team[0]}'"""
-       cur.execute(postgresSQL_autoamp_Query)
-       datapoints = cur.fetchall()  
-       # print(datapoints)
-       total = 0
+    for team in teams:
+        postgresSQL_autoamp_Query = f"""SELECT auto_amp_succeed FROM "TeamMatches" WHERE team_key='{team[0]}'"""
+        cur.execute(postgresSQL_autoamp_Query)
+        datapoints = cur.fetchall()  
+        # print(datapoints)
+        total = 0
 
 
-       for datapoint in datapoints:
-           total += datapoint[0]
+        for datapoint in datapoints:
+            total += datapoint[0]
 
 
-           autoamps[team] = round(total / len(datapoints), 2)
+            autoamps[team] = round(total / len(datapoints), 2)
 
 
 
 
-   descending_autoamps = sorted(autoamps.items(), key=operator.itemgetter(1))
-   descending_autoamps = dict( sorted(autoamps.items(), key=operator.itemgetter(1), reverse=True))
+    descending_autoamps = sorted(autoamps.items(), key=operator.itemgetter(1))
+    descending_autoamps = dict( sorted(autoamps.items(), key=operator.itemgetter(1), reverse=True))
 
 
        # print(team, total)
@@ -67,49 +67,49 @@ def convert_to_html():
 
 
 
-   keys = [key[0] for key in list(descending_autoamps.keys())]
-   values = [value for value in list(descending_autoamps.values())]
+    keys = [key[0] for key in list(descending_autoamps.keys())]
+    values = [value for value in list(descending_autoamps.values())]
 
 
-   # print(keys)
-   # print(values)
+    # print(keys)
+    # print(values)
 
 
-   fig = go.Figure(go.Bar(x=keys, y=values))
+    fig = go.Figure(go.Bar(x=keys, y=values))
 
 
-   # # Execute a query
-   # postgreSQL_select_Query = 'SELECT team_key, auto_amp_succeed FROM "TeamMatches"'
-   # cur.execute(postgreSQL_select_Query)
+    # # Execute a query
+    # postgreSQL_select_Query = 'SELECT team_key, auto_amp_succeed FROM "TeamMatches"'
+    # cur.execute(postgreSQL_select_Query)
 
 
-   # teamnames_Query = 'SELECT team_key FROM "Teams"'
-   # cur.execute(teamnames_Query)
+    # teamnames_Query = 'SELECT team_key FROM "Teams"'
+    # cur.execute(teamnames_Query)
 
-   # # Get the results
-   # rows = cur.fetchmany(50)
-
-
-   # print(rows)
+    # # Get the results
+    # rows = cur.fetchmany(50)
 
 
-   # teamnums = [row[0] for row in rows]
-   # autoamps = [row[1] for row in rows]
+    # print(rows)
 
 
-   # fig = go.Figure([go.Bar (x=teamnums, y=autoamps)])
-   # fig.show()
+    # teamnums = [row[0] for row in rows]
+    # autoamps = [row[1] for row in rows]
 
 
-   # Close the cursor and the connection
-   cur.close()
-   conn.close()
+    # fig = go.Figure([go.Bar (x=teamnums, y=autoamps)])
+    # fig.show()
 
 
-   html = fig.to_html (
-       include_plotlyjs=False,
-       full_html=False,
-   )
+    # Close the cursor and the connection
+    cur.close()
+    conn.close()
 
 
-   return html
+    html = fig.to_html (
+        include_plotlyjs=False,
+        full_html=False,
+    )
+
+
+    return html
