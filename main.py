543df6a2b904e90e5879380e25e3eab0bff5d434
YayBlaze <blazeannison@icloud.com>
2024-02-24 15:09:53 -0800

    feat: added slide for total # of traps scored

--- main.py
+++ main.py
@@ -427,6 +427,17 @@ def times_climb():
     postgresSQL_1540_climb_Query = f"""SELECT stage_enum FROM "TeamMatches" WHERE team_key='1540'"""
     cur.execute(postgresSQL_1540_climb_Query)
     stage = cur.fetchall()
+def trap_num():
+    postgresSQL_trap_suc_Query = """SELECT trap_succeed FROM "TeamMatches" WHERE team_key='1540'"""
+    cur.execute(postgresSQL_trap_suc_Query)
+    trap = cur.fetchall()
+    total = 0
+    for i in trap:
+        total += i[0]
+    if total > 1:
+        return f"<h4>Team 1540's robot has scored</h4><h2>{total}</h2><h4>notes in trap this season</h4>"
+    else:
+        return "bad"
 def message1():
     return "<h2>Thank You</h2><h4>for visiting team 1540's pits</h4>"
 def message2():
@@ -434,7 +445,8 @@ def message2():
 def make_graph() -> str:
 
 
-    listofresults=[get_trap_graph(), get_amp_graph(), get_speaker_graph(), message2(), auto_acc_graph(), get_broke_graph(), get_total_auto(), get_total_whole(), message1()]
+    # listofresults=[get_trap_graph(), get_amp_graph(), get_speaker_graph(), message2(), auto_acc_graph(), get_broke_graph(), get_total_auto(), get_total_whole(), message1(), trap_num()]
+    listofresults=[trap_num()]
     reallist = []
     for result in listofresults:
         if result != "bad":
