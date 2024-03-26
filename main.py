a2c78e31bb5ff81562d3ef4cf5fae4d4e6a0095e
YayBlaze <blazeannison@icloud.com>
2024-03-25 19:35:00 -0700

    feat: did things, forgot what they were tho

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ def feather_message(toggle):
         return "<h4>There are as many people that do FIRST as feathers on </h4><h2>Ten and Half chickens!</h2>"  
     else: return 'bad'
 def chicken_notes(toggle):
-    if toggle: return "<h4 style='display:inline-block;'>A chicken of the </h4><p style='display:inline-block;'></p><h4 style='color: #FFC145; display:inline-block;'> non-flaming</h4><h4>variety is expected to score</h4><h3>0</h3><h4>notes during a match!</h4>"
+    if toggle: return "<h4 style='display:inline-block;'>A chicken of the </h4><h4 style='color: #FFC145; display:inline-block; padding-left: 1rem;'> non-flaming</h4><h4>variety is expected to score</h4><h3>0</h3><h4>notes during a match!</h4>"
     else: return 'bad'
 def chicken_weight(toggle):
     if toggle: return '<h4>The heaviest chicken was 22lbs! That is</h4><h3>88 lbs</h3><h4>less than the weight of our robot!</h4>'
@@ -289,11 +289,13 @@ def event_acc(toggle, html):
         miss_speaker = sum_auto_speaker_miss() + sum_tele_speaker_miss()
         missed = miss_speaker + miss_amp
         shots = scored + missed
-        acc = round(scored / missed, 3)
-        if html: return f'<h4>Teams at this event have scored</h4><h3>{acc * 100}%</h3><h4>of total notes shot!</h4>'
+        acc = round(scored / shots, 3)
+        # print('\n\nscored:',scored,'\nmiss_amp:',miss_amp,'\nmiss_speaker:',miss_speaker,'\nmissed:',missed,'\nshots:',shots,'\nacc:',acc,'\nprint:',acc * 100,'\n\n')
+        if html: return f'<h4>Teams at this event have scored</h4><h3>{round(acc * 100, 1)}%</h3><h4>of total notes shot!</h4>'
+        else: return acc
     else: return 'bad'
 def chicken_foul():
-    return "<h1>However, in the whimsical scenario where we decide to replace the robot on the field with a live chicken, a cascade of unforeseen consequences would likely unfold. Picture this: amidst the high-stakes game, the unsuspecting chicken, blissfully unaware of the intricate rules governing the match, would likely become the unwitting perpetrator of an array of tech fouls. The referee, undoubtedly perplexed by the surreal turn of events, might find themselves compelled to brandish a red card, signaling not only an expulsion from the game but also drawing attention to the peculiar nature of the infringement.</h1>"
+    return "<h1>However, in the whimsical scenario where we decide to replace the robot on the field with a live chicken, a cascade of unforeseen consequences would likely unfold. Picture this: amidst the high-stakes game, the unsuspecting chicken, blissfully unaware of the intricate rules governing the match, would likely become the unwitting perpetrator of an array of tech fouls. The referee, undoubtedly perplexed by the surreal turn of events, might find themselves compelled to brandish a red card, signaling not only an expulsion from the game but also drawing attention to the peculiar nature of the infringement.</h1><marquee><marquee>"
 def chicken_noise(toggle):
     if toggle: return "<h4>Chickens can squawk as loud as</h4><h2>70 decibels<h2><h4>about as loud as the average classroom</h4>"
     else: return 'bad'
@@ -305,14 +307,16 @@ def chicken_eat(toggle):
     else: return 'bad'
 def chicken_cycles(toggle):
     if toggle:
-        return '<h4 style="display:inline-block;">A chicken of the </h4><p style="display:inline-block;"> </p><h4 style="display:inline-block; color: #FFC145;"> non-flaming</h4><h4>variety can make a speaker cycle in</h4><h2>3 seconds!</h2>'
+        return '<h4 style="display:inline-block;">A chicken of the </h4>><h4 style="display:inline-block; color: #FFC145; padding-left: 1rem;"> non-flaming</h4><h4>variety can make a speaker cycle in</h4><h2>3 seconds!</h2>'
     else: return 'bad'
 def socials(toggle):
-    if toggle: return '<div><h6>Team Website</h6><img src="https://i.ibb.co/dphZnQt/teamwebqr.png" alt="THIS IS ALT TEXT" id="bigimg"></div><div class="grid-container"><div><h5>Team Instagram</h5><img src="https://i.ibb.co/7nmGL3T/qr-code-1.png" alt="qr code" id="smallimg"></div><div><h5>Team YouTube</h5><img src="https://i.ibb.co/LnywZNv/qr-code-2.png" alt="qr code" id="smallimg"></div></div>'
+    if toggle: return '<div><h6>Team Website</h6><img src="https://i.ibb.co/dphZnQt/teamwebqr.png" alt="THIS IS ALT TEXT" id="bigimg"></div><div class="grid-container"><div><h5>Team Instagram</h5><img src="https://i.ibb.co/7nmGL3T/qr-code-1.png" alt="qr code" id="smallimg"></div><div><h5>Team YouTube</h5><img src="https://i.ibb.co/LnywZNv/qr-code-2.png" alt="qr code" id="smallimg"></div></div><marquee></marquee>'
     else: return 'bad'
 def logodvd(toggle):
     if toggle: return '<marquee class="marquee" behavior="alternate" direction="down"scrollamount="20"><marquee behavior="alternate" width="100%" scrollamount="20"><img width="250px" src="https://avatars.githubusercontent.com/u/5280254?s=200&v=4" alt="dvd" id="spin"></marquee></marquee>'
     else: return 'bad'
+
+
 
 def make_graph() -> list[str]:
     listofresults=[eggs_in_match(toggle_list[1]), feather_message(toggle_list[2]), chicken_notes(toggle_list[3]), chicken_weight(toggle_list[4]), eggs_in_season(toggle_list[5], True), robo_name(toggle_list[6]), event_scored(toggle_list[7],True),event_acc(toggle_list[8], True), chicken_noise(toggle_list[9]), party_parrot(toggle_list[10]), chicken_eat(toggle_list[11]), chicken_cycles(toggle_list[12]), socials(toggle_list[13]), logodvd(toggle_list)]
@@ -357,8 +361,8 @@ def main():
             # print('\n\nchoice:',robo_name(True),'\nold:',old,'\nold2:',old2,'old3:',old3,'\n\n')
             old3 = old2
             old2 = old
-            old = robo_name(True)
-            return robo_name(True)
+            old = logodvd(True)
+            return logodvd(True)
     # if len(html)-1 > listindex:
     #     listindex +=1
     # else:
