861b8aef4873d065a85f3cd8c9dcf6312fcfa0e4
David :D <KongD@catlin.edu>
2024-04-02 17:02:07 -0700

    feat: Blue Alliance API data implementation :D

---
+++ MatchData.py
@@ -0,0 +1,91 @@
+import numpy as np
+from tbaapiv3client import Zebra
+
+
+def getTeamDistance(team):
+    teamX = team.xs
+    teamY = team.ys
+    teamDistance = 0
+    i = 0
+    while i < len(teamX) - 1:
+        nextIndex = i + 1
+        while nextIndex + 1 < len(teamX) - 1 and (teamX[i] is None or teamY[i] is None or teamX[nextIndex] is None or
+                                                  teamY[nextIndex] is None):
+            nextIndex = nextIndex + 1
+        if not (teamX[i] is None or teamY[i] is None or teamX[nextIndex] is None or
+                teamY[nextIndex] is None):
+            teamDistance = teamDistance + np.hypot(teamX[nextIndex] - teamX[i], teamY[nextIndex] - teamY[i])
+        i = nextIndex
+    return teamDistance
+
+
+class MatchData:
+    # def __init__(self, autoAmpNotes, autoSpeakerNotes, teleAmpNotes, teleSpeakerNotes, trapNotes, amplifiedSpeakerNotes, numberOfSpotlights, redScore, blueScore, maxScore, totalAmpNotes, totalSpeakerNotes, totalNotes):
+
+    def __init__(self, match, zebraData):
+        self.matchKey = match.key
+        self.scoringData = match
+        scoreBreakdown = self.scoringData.score_breakdown
+        self.zebraData = zebraData
+        self.totalDistanceTraveled = 0
+        if scoreBreakdown is not None:
+            red = scoreBreakdown['red']
+            blue = scoreBreakdown['blue']
+            self.autoAmpNotes = red['autoAmpNoteCount'] + blue['autoAmpNoteCount']
+            self.autoSpeakerNotes = red['autoSpeakerNoteCount'] + blue['autoSpeakerNoteCount']
+            self.teleAmpNotes = red['teleopAmpNoteCount'] + blue['teleopAmpNoteCount']
+            self.teleSpeakerNotes = red['teleopSpeakerNoteCount'] + blue['teleopSpeakerNoteCount']
+            self.trapNotes = (red['endGameNoteInTrapPoints'] / 5) + (blue['endGameNoteInTrapPoints'] / 5)
+            self.amplifiedSpeakerNotes = red['teleopSpeakerNoteAmplifiedCount'] + blue[
+                'teleopSpeakerNoteAmplifiedCount']
+            self.numberOfSpotlights = 0
+            if red['micCenterStage']:
+                self.numberOfSpotlights = self.numberOfSpotlights + 1
+            if red['micStageLeft']:
+                self.numberOfSpotlights = self.numberOfSpotlights + 1
+            if red['micStageRight']:
+                self.numberOfSpotlights = self.numberOfSpotlights + 1
+            if blue['micCenterStage']:
+                self.numberOfSpotlights = self.numberOfSpotlights + 1
+            if blue['micStageLeft']:
+                self.numberOfSpotlights = self.numberOfSpotlights + 1
+            if blue['micStageRight']:
+                self.numberOfSpotlights = self.numberOfSpotlights + 1
+
+            self.redScore = red['totalPoints']
+            self.blueScore = blue['totalPoints']
+            self.maxScore = max(self.redScore, self.blueScore)
+
+            self.totalAmpNotes = self.autoAmpNotes + self.teleAmpNotes
+            self.totalSpeakerNotes = self.autoSpeakerNotes + self.teleSpeakerNotes
+            self.totalNotes = self.totalAmpNotes + self.totalSpeakerNotes + self.trapNotes
+        else:
+            self.autoAmpNotes = 0
+            self.autoSpeakerNotes = 0
+            self.teleAmpNotes = 0
+            self.teleSpeakerNotes = 0
+            self.trapNotes = 0
+            self.amplifiedSpeakerNotes = 0
+            self.numberOfSpotlights = 0
+            self.redScore = 0
+            self.blueScore = 0
+            self.maxScore = 0
+            self.totalAmpNotes = 0
+            self.totalSpeakerNotes = 0
+            self.totalNotes = 0
+
+    def computeDistance(self):
+        if self.zebraData is not None:
+            self.totalDistanceTraveled = 0
+
+            team1Zebra = self.zebraData.alliances.blue[0]
+            team2Zebra = self.zebraData.alliances.blue[1]
+            team3Zebra = self.zebraData.alliances.blue[2]
+            team4Zebra = self.zebraData.alliances.red[0]
+            team5Zebra = self.zebraData.alliances.red[1]
+            team6Zebra = self.zebraData.alliances.red[2]
+
+            self.totalDistanceTraveled = getTeamDistance(team1Zebra) + getTeamDistance(team2Zebra) + getTeamDistance(
+                team3Zebra) + getTeamDistance(team4Zebra) + getTeamDistance(team5Zebra) + getTeamDistance(team6Zebra)
+        else:
+            self.totalDistanceTraveled = 0
---
+++ TBAData.py
@@ -0,0 +1,131 @@
+import time
+import pickle
+import multiprocessing as multip
+import numpy as np
+
+import tbaapiv3client
+
+from MatchData import MatchData
+
+
+class TBAData:
+    def __init__(self):
+        # API Key Authorization
+        configuration = tbaapiv3client.Configuration(
+            host="https://www.thebluealliance.com/api/v3",
+            api_key={
+                'X-TBA-Auth-Key': '***REMOVED***'
+            }
+        )
+
+        # Get API instances
+        with tbaapiv3client.ApiClient(configuration) as api_client:
+            self.matches_api_instance = tbaapiv3client.EventApi(api_client)
+            self.match_api_instance = tbaapiv3client.MatchApi(api_client)
+            self.event_api_instance = tbaapiv3client.EventApi(api_client)
+
+        # Set constants
+        self.AT_EVENT_KEY = "2024camb"
+        self.CURRENT_EVENT_KEYS = ["2024camb"]
+        self.CURRENT_YEAR_KEY = "2024"
+        self.EVENT_KEYS = self.event_api_instance.get_events_by_year_keys(int(self.CURRENT_YEAR_KEY))
+        # self.EVENT_KEYS = self.CURRENT_EVENT_KEYS
+        # Initialize Fields
+        self.event_to_match_data = {}
+        self.current_event_data = []
+        self.at_event_data = []
+
+    def load_events(self, file):
+        print("Loading Events...")
+        currentTime = time.time()
+
+        eventsDone = 0
+        totalEvents = len(self.EVENT_KEYS)
+
+        match_to_thread = {}
+
+        for eventKey in self.EVENT_KEYS:
+            print("Requesting Event: " + eventKey)
+            matches = self.get_event_matches(eventKey)
+            for match in matches:
+                match_to_thread[match.key] = self.match_api_instance.get_match_zebra(match.key, async_req=True)
+            eventsDone = eventsDone + 1
+            print(eventsDone / totalEvents * 100, "% of Events Requested")
+            print("--------------------------------------------------------")
+        print("Loading Event Matches Took: " + str(time.time() - currentTime))
+        currentTime = time.time()
+        eventsDone = 0
+        for eventKey in self.EVENT_KEYS:
+            self.event_to_match_data[eventKey] = {}
+            for match in self.get_event_matches(eventKey):
+                self.event_to_match_data[eventKey][match.key] = MatchData(match, match_to_thread[match.key].get())
+            eventsDone = eventsDone + 1
+            print(eventsDone / totalEvents * 100, "% of Events Loaded")
+        self.current_event_data = [self.event_to_match_data[key] for key in self.CURRENT_EVENT_KEYS]
+
+        self.at_event_data = self.event_to_match_data[self.AT_EVENT_KEY]
+
+        print("Loading Event Matches Took: " + str(time.time() - currentTime) + " Seconds")
+        print("Writing to File...")
+        self.write_to_file(file)
+        print("Writing to File Complete")
+
+    def get_event_matches(self, event_key):
+        matches = self.matches_api_instance.get_event_matches(event_key)
+        return matches
+
+
+    def write_to_file(self, file):
+        dataFile = open(file, "wb")
+        pickle.dump(self.event_to_match_data, dataFile)
+
+        dataFile.close()
+
+    def load_from_file(self, file):
+        currentTime = time.time()
+        print("Loading Data From File...")
+        dataFile = open(file, "rb")
+        self.event_to_match_data = pickle.load(dataFile)
+        dataFile.close()
+        self.current_event_data = [self.event_to_match_data[key] for key in self.CURRENT_EVENT_KEYS]
+        self.at_event_data = self.event_to_match_data[self.AT_EVENT_KEY]
+        print("Loading Data From File Complete Took: " + str(time.time() - currentTime) + " Seconds")
+
+    def update_current_event_data(self, file):
+        print("Updating Data...")
+        currentTime = time.time()
+
+        eventsDone = 0
+        totalEvents = len(self.CURRENT_EVENT_KEYS)
+
+        match_to_thread = {}
+
+        for eventKey in self.CURRENT_EVENT_KEYS:
+            print("Requesting Event: " + eventKey)
+            matches = self.get_event_matches(eventKey)
+            for match in matches:
+                match_to_thread[match.key] = self.match_api_instance.get_match_zebra(match.key, async_req=True)
+            eventsDone = eventsDone + 1
+            print(eventsDone / totalEvents * 100, "% of Events Requested")
+            print("--------------------------------------------------------")
+        print("Loading Event Matches Took: " + str(time.time() - currentTime))
+        currentTime = time.time()
+        eventsDone = 0
+        for eventKey in self.CURRENT_EVENT_KEYS:
+            for match in self.get_event_matches(eventKey):
+                self.event_to_match_data[eventKey] = {}
+                self.event_to_match_data[match.key] = MatchData(match, match_to_thread[match.key].get())
+            eventsDone = eventsDone + 1
+            print(eventsDone / totalEvents * 100, "% of Events Loaded")
+        self.current_event_data = [self.event_to_match_data[key] for key in self.CURRENT_EVENT_KEYS]
+
+        self.at_event_data = self.event_to_match_data[self.AT_EVENT_KEY]
+
+        print("Loading Event Matches Took: " + str(time.time() - currentTime) + " Seconds")
+        print("Writing to File...")
+        self.write_to_file(file)
+        print("Writing to File Complete")
+        print("Updated Current Event Data(took " + str(time.time() - currentTime) + " Seconds)")
+
+    def get_match_data(self, event_key, match_key):
+        return self.event_to_match_data[event_key][match_key]
