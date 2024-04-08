c14472980c64f70c0808e5cf6508a0fbbcefc027
David :D <KongD@catlin.edu>
2024-04-07 18:55:17 -0700

    feat: working live event data updating and everything else :D

--- SlideHTMLGenerators.py
+++ SlideHTMLGenerators.py
@@ -2,6 +2,7 @@ import time
 
 from main import hensightStats
 
+
 def thank_msg(toggle):
     if toggle:
         return '<h4>Team 1540 Thanks</h4><h3>YOU</h3><h4>for joining us in our pits!</h4>'
@@ -11,7 +12,7 @@ def thank_msg(toggle):
 
 def eggs_in_match(toggle):
     if toggle:
-        return "<h4>During each match over</h4><h3 id='h7'>520,000</h3><h4>eggs are laid in the US!</h4>"
+        return "<h4>During each match over</h4><h2 style='font-size:13rem;'i>520,000</h2><h4>eggs are laid in the US!</h4>"
     else:
         return 'bad'
 
@@ -59,7 +60,7 @@ def robo_name(toggle):
 
 
 def chicken_foul():
-    return "<h1>However, in the whimsical scenario where we decide to replace the robot on the field with a live chicken, a cascade of unforeseen consequences would likely unfold. Picture this: amidst the high-stakes game, the unsuspecting chicken, blissfully unaware of the intricate rules governing the match, would likely become the unwitting perpetrator of an array of tech fouls. The referee, undoubtedly perplexed by the surreal turn of events, might find themselves compelled to brandish a red card, signaling not only an expulsion from the game but also drawing attention to the peculiar nature of the infringement.</h1>"
+    return "<h1 style='font-size:1.5rem;'>However, in the whimsical scenario where we decide to replace the robot on the field with a live chicken, a cascade of unforeseen consequences would likely unfold. Picture this: amidst the high-stakes game, the unsuspecting chicken, blissfully unaware of the intricate rules governing the match, would likely become the unwitting perpetrator of an array of tech fouls. The referee, undoubtedly perplexed by the surreal turn of events, might find themselves compelled to brandish a red card, signaling not only an expulsion from the game but also drawing attention to the peculiar nature of the infringement.</h1>"
 
 
 def chicken_noise(toggle):
@@ -71,7 +72,7 @@ def chicken_noise(toggle):
 
 def chicken_eat(toggle):
     if toggle:
-        return '<h4>FRC Students eat approximately</h4><h3 style="font-size: 13rem;">7,917,000</h3><h4>chickens per year</h4>'
+        return '<h4>FRC Students eat approximately</h4><h3 style="font-size: 11rem;">7,917,000</h3><h4>chickens per year</h4>'
     else:
         return 'bad'
 
@@ -85,7 +86,7 @@ def chicken_cycles(toggle):
 
 def battery(toggle):
     if toggle:
-        return '<h5>A chicken running on a hamster wheel would take</h5><h3 id="h7">5 hours</h3><h5>to generate enough power for a ES 17-12 battery</h5>'
+        return '<h5>A chicken running on a hamster wheel would take</h5><h3 style="font-size:13rem;">5 hours</h3><h5>to generate enough power for a ES 17-12 battery</h5>'
     else:
         return 'bad'
 
@@ -96,24 +97,74 @@ def logodvd(toggle):
     else:
         return 'bad'
 
-def event_total_notes(toggle):
-    if toggle:
-        return f'<h4>There has been</h4><h3>{round(hensightStats.hensight_stats["event_total_notes"])}</h3><h4>notes scored at this event</h4>'
-    else: return 'bad'
-    
-def event_trap_notes(toggle):
-    if toggle:
+
+def event_total_notes(toggle, event_toggle):
+    if toggle and event_toggle:
+        return f'<h4>There has been</h4><h3>{"{:,}".format(round(hensightStats.hensight_stats["event_total_notes"]))}</h3><h4>notes scored at this event</h4>'
+    else:
+        return 'bad'
+
+
+def event_trap_notes(toggle, event_toggle):
+    if toggle and event_toggle:
         return f'<h4>Teams have scored</h4><h3>{round(hensightStats.hensight_stats["event_trap_notes"])}</h3><h4>notes in the trap at this event</h4>'
-    else: return 'bad'
-def event_high_score(toggle):
-    if toggle:
+    else:
+        return 'bad'
+
+
+def event_high_score(toggle, event_toggle):
+    if toggle and event_toggle:
         return f'<h4>The high score at this event is</h4><h3>{round(hensightStats.hensight_stats["event_high_score"])}</h3><h4>points! (good job)</h4>'
-    else: return 'bad'
-def spotlight_percent(toggle):
-    if toggle:
-        return f'<h4>At this competition</h4><h3>{round(hensightStats.hensight_stats["event_spotlight_note_percentage"], 2)}%<h3><h4>of climbed robotics have been spotlit'
-    else: return 'bad'
+    else:
+        return 'bad'
+
+
+def spotlight_percent(toggle, event_toggle):
+    if toggle and event_toggle:
+        return f'<h1>At this competition</h1><h3>{round(hensightStats.hensight_stats["event_spotlight_note_percentage"], 2)}%<h3><h1>of robotics have been spotlit</h1><div></div><h1>In the world</h1><h3>{round(hensightStats.hensight_stats["global_spotlight_note_percentage"], 2)}%</h3><h1>of robots have been spotlit</h1>'
+    else:
+        return 'bad'
+
+
 def global_total_notes(toggle):
     if toggle:
-        return f'<h4>In the world</h4><h3>{round(hensightStats.hensight_stats["global_total_notes"])}</h3><h4>notes have been scored in total</h4>'
-    else: return 'bad'
+        return f'<h4>In the world</h4><h2>{"{:,}".format(round(hensightStats.hensight_stats["global_total_notes"]))}</h2><h4>notes have been scored</h4>'
+    else:
+        return 'bad'
+
+
+def global_high_score(toggle):
+    if toggle:
+        return f"<h4>The global high score is</h4><h3>{round(hensightStats.hensight_stats["global_high_score"])}</h3><h4>points! That's a lot"
+    else:
+        return 'bad'
+
+
+def event_speaker_notes(toggle, event_toggle):
+    if toggle and event_toggle:
+        return f'<h1>At this event, teams have scored</h1><h3>{"{:,}".format(round(hensightStats.hensight_stats["event_speaker_notes"]))}</h3><h1>notes in the speaker, while having scored</h1><h3>{round(hensightStats.hensight_stats["event_amplified_speaker_notes"])}</h3><h1>notes in an amplified speaker</h1>'
+    else:
+        return 'bad'
+
+
+def global_amp_notes(toggle):
+    if toggle:
+        return f'<h4>Throughout the world</h4><h3 id="h7">{"{:,}".format(round(hensightStats.hensight_stats["global_amp_notes"]))}</h3><h4>notes have been scored in the amp</h4>'
+    else:
+        return 'bad'
+
+
+def event_travel(toggle, event_toggle):
+    if toggle and event_toggle:
+        return f'<h4>Robots at this event have traveled</h4><h2>{"{:,}".format(round(hensightStats.hensight_stats["event_distance_traveled"]))}ft</h2><h4>in total :O</h4>'
+    else:
+        return 'bad'
+
+
+def global_travel(toggle):
+    if toggle:
+        return f'<h4>All the robots in the world have traveled</h4><h2>{"{:,}".format(round(hensightStats.hensight_stats["global_distance_traveled"]))}ft</h2><h4>in total :O</h4>'
+
+def event_alliance_score(toggle, event_toggle):
+    if toggle and event_toggle:
+        return f'<h4>At this event, the blue alliance has scored</h4><h2>{"{:,}".format(round(hensightStats.hensight_stats["event_blue_alliance_score"]))}</h2><h4>points combined</h4><h4>While the red alliance has scored</h4><h2>{"{:,}".format(round(hensightStats.hensight_stats["event_red_alliance_score"]))}</h2><h4>points combined</h4>'
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 from flask import request
-import time
+# import time
 import random
-
+import schedule
+import threading
 from flask import Flask, render_template, send_file
 from flask import request
 
@@ -23,24 +24,37 @@ TBAData = TBAData()
 #
 #
 TBAData.load_from_file("data.txt")
-# TBAData.update_current_event_data("SaturdayTest.txt")
+# TBAData.update_current_event_data("data.txt")
+
 # #
 hensightStats = HensightStatsManager(TBAData)
 hensightStats.update_stats()
 #
+def update_this_event():
+    print("---updating---")
+    TBAData.update_current_event_data("data.txt")
+    print("---update finished---")
 
+schedule.every(5).minutes.do(update_this_event)
 
-
-
+def loop():
+    # print("-fnc run-")
+    while True:
+        # print("-loop run-")
+        schedule.run_pending()
+        time.sleep(1)
+b = threading.Thread(name='loop', target=loop)
+b.start()
 
 def make_graph() -> list[str]:
     listofresults = [
                         eggs_in_match(toggle_list[1]), feather_message(toggle_list[2]), chicken_notes(toggle_list[3]),
                         chicken_weight(toggle_list[4]), eggs_in_season(toggle_list[5], True), robo_name(toggle_list[6]),
                         chicken_noise(toggle_list[7]), chicken_eat(toggle_list[8]), chicken_cycles(toggle_list[9]),
-                        battery(toggle_list[10]), logodvd(toggle_list[11]), event_total_notes(toggle_list[12]), 
-                        event_trap_notes(toggle_list[13]), event_high_score(toggle_list[14]), spotlight_percent(toggle_list[15]),
-                        global_total_notes(toggle_list[16])
+                        battery(toggle_list[10]), logodvd(toggle_list[11]), event_total_notes(toggle_list[12], event_toggle),
+                        event_trap_notes(toggle_list[13], event_toggle), event_high_score(toggle_list[14], event_toggle), spotlight_percent(toggle_list[15], event_toggle),
+                        global_total_notes(toggle_list[16]), global_high_score(toggle_list[17]), event_speaker_notes(toggle_list[18], event_toggle),
+                        global_amp_notes(toggle_list[19]), event_travel(toggle_list[20], event_toggle), global_travel(toggle_list[21]), event_alliance_score(toggle_list[22], event_toggle)
                      ]
     reallist = []
     for result in listofresults:
@@ -48,8 +62,8 @@ def make_graph() -> list[str]:
             reallist.append(result)
     return reallist
 
-
-toggle_list = [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]
+event_toggle = True
+toggle_list = [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, True, True, True, True, False, True, True]
 
 
 @app.route("/")
@@ -73,7 +87,7 @@ def serve_obje_file():
 
 @app.route('/reveal')
 def reveal():
-    return render_template('reveal.html')   
+    return render_template('reveal.html')
 
 @app.route('/request')
 def main():
@@ -136,5 +150,5 @@ def CHICKEN_SPIN():
     return render_template('chickenspin.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5001, debug=True, use_reloader=True)
     print("hi")
