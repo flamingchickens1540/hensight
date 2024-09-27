4e2b2548126a0897047168a0624607276c07cf63
awwpotato <153149335+awwsomepotato@users.noreply.github.com>
2024-09-26 18:55:47 -0700

    chore: move some stuff to common js file

---
+++ templates/common.js
@@ -0,0 +1,10 @@
+let elem = document.documentElement;
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
