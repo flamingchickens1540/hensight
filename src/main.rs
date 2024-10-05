78efd5d92ba37e6843cd0b093bc5de729577d06f
awwpotato <153149335+awwsomepotato@users.noreply.github.com>
2024-10-05 16:17:14 -0700

    chore: formating thingy

--- src/main.rs
+++ src/main.rs
@@ -32,41 +32,20 @@ async fn main() {
     );
 
     // closure so tba_api can be used by them in the future
+    #[rustfmt::skip]
     let slides = vec![
         |_| Some("<h4>Team 1540 Thanks</h4><h3>YOU</h3><h4>for joining us in our pits!</h4>"),
-        |_| {
-            Some("<h4>During each match over</h4><h2 style='font-size:13rem;'i>520,000</h2><h4>eggs are laid in the US!</h4>")
-        },
-        |_| {
-            Some("<h4>There are as many people that do FIRST as feathers on </h4><h2>Ten and Half chickens!</h2>")
-        },
-        |_| {
-            Some("<h4>A chicken of the non-flaming variety is expected to score</h4><h3>0</h3><h4>notes during a match!</h4>")
-        },
-        |_| {
-            Some("<h4>The heaviest chicken was 22lbs! That is</h4><h3>93 lbs</h3><h4>less than the weight of our robot!</h4>")
-        },
-        |_| {
-            Some("<h4>Our robot is named Fried Egg because</h4><h2>We're cooking</h2><h4>this year!</h4>")
-        },
-        |_| {
-            Some("<h1 style='font-size:3rem;'>However, in the whimsical scenario where we decide to replace the robot on the field with a live chicken, a cascade of unforeseen consequences would likely unfold. Picture this: amidst the high-stakes game, the unsuspecting chicken, blissfully unaware of the intricate rules governing the match, would likely become the unwitting perpetrator of an array of tech fouls. The referee, undoubtedly perplexed by the surreal turn of events, might find themselves compelled to brandish a red card, signaling not only an expulsion from the game but also drawing attention to the peculiar nature of the infringement.</h1>")
-        },
-        |_| {
-            Some("<h4>Chickens can squawk as loud as</h4><h2>70 decibels<h2><h4>about as loud as the average classroom</h4>")
-        },
-        |_| {
-            Some("<h4>FRC Students eat approximately</h4><h3 style=\"font-size: 11rem;\">7,917,000</h3><h4>chickens per year</h4>")
-        },
-        |_| {
-            Some("<h4>A chicken of the non-flaming variety can make a speaker cycle in</h4><h2>3 seconds!</h2>")
-        },
-        |_| {
-            Some("<h5>A chicken running on a hamster wheel would take</h5><h3 style=\"font-size:13rem;\">5 hours</h3><h5>to generate enough power for a ES 17-12 battery</h5>")
-        },
-        |_| {
-            Some("<marquee class=\"marquee\" behavior=\"alternate\" direction=\"down\"scrollamount=\"20\" id=\"logo\"><marquee style=\"margin-bottom: 160px\" behavior=\"alternate\" width=\"100%\" scrollamount=\"20\"><img width=\"250px\" src=\"https://avatars.githubusercontent.com/u/5280254?s=200&v=4\" alt=\"dvd\" id=\"spin\"></marquee></marquee><p id=\"msg\">inside of my head rn</p>")
-        },
+        |_| Some("<h4>During each match over</h4><h2 style='font-size:13rem;'i>520,000</h2><h4>eggs are laid in the US!</h4>"),
+        |_| Some("<h4>There are as many people that do FIRST as feathers on </h4><h2>Ten and Half chickens!</h2>"),
+        |_| Some("<h4>A chicken of the non-flaming variety is expected to score</h4><h3>0</h3><h4>notes during a match!</h4>"),
+        |_| Some("<h4>The heaviest chicken was 22lbs! That is</h4><h3>93 lbs</h3><h4>less than the weight of our robot!</h4>"),
+        |_| Some("<h4>Our robot is named Fried Egg because</h4><h2>We're cooking</h2><h4>this year!</h4>"),
+        |_| Some("<h1 style='font-size:3rem;'>However, in the whimsical scenario where we decide to replace the robot on the field with a live chicken, a cascade of unforeseen consequences would likely unfold. Picture this: amidst the high-stakes game, the unsuspecting chicken, blissfully unaware of the intricate rules governing the match, would likely become the unwitting perpetrator of an array of tech fouls. The referee, undoubtedly perplexed by the surreal turn of events, might find themselves compelled to brandish a red card, signaling not only an expulsion from the game but also drawing attention to the peculiar nature of the infringement.</h1>"),
+        |_| Some("<h4>Chickens can squawk as loud as</h4><h2>70 decibels<h2><h4>about as loud as the average classroom</h4>"),
+        |_| Some("<h4>FRC Students eat approximately</h4><h3 style=\"font-size: 11rem;\">7,917,000</h3><h4>chickens per year</h4>"),
+        |_| Some("<h4>A chicken of the non-flaming variety can make a speaker cycle in</h4><h2>3 seconds!</h2>"),
+        |_| Some("<h5>A chicken running on a hamster wheel would take</h5><h3 style=\"font-size:13rem;\">5 hours</h3><h5>to generate enough power for a ES 17-12 battery</h5>"),
+        |_| Some("<marquee class=\"marquee\" behavior=\"alternate\" direction=\"down\"scrollamount=\"20\" id=\"logo\"><marquee style=\"margin-bottom: 160px\" behavior=\"alternate\" width=\"100%\" scrollamount=\"20\"><img width=\"250px\" src=\"https://avatars.githubusercontent.com/u/5280254?s=200&v=4\" alt=\"dvd\" id=\"spin\"></marquee></marquee><p id=\"msg\">inside of my head rn</p>"),
     ];
 
     let app = Router::new()
