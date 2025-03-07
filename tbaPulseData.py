ff899be7640a746bb3bbc5436e7311f626c9e611
YayBlaze <blazeannison@icloud.com>
2025-03-07 10:11:53 -0800

    pain

--- tbaPulseData.py
+++ tbaPulseData.py
@@ -90,9 +90,10 @@ def getMatchSchedule():
     matches = tba.event_matches(event=event_key, simple=True)
     allMatches = [format(matches)]
     futureMatches = []
+    print(matches[0])
     for i in matches:
         if "winning_alliance" == "": futureMatches.append(i)
-    if len(futureMatches) > 1: futureMatches = [format(futureMatches)]
+    if len(futureMatches) > 0: futureMatches = [format(futureMatches)]
     data = {
         "all": allMatches,
         "future": futureMatches
