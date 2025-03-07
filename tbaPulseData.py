61e1b4f56e889115dc3979812f3d0edeabceba56
YayBlaze <blazeannison@icloud.com>
2025-03-07 14:26:41 -0800

    trying to get future matches to work

--- tbaPulseData.py
+++ tbaPulseData.py
@@ -95,7 +95,9 @@ def getMatchSchedule():
     futureMatches = []
     for i in matches:
         if i["winning_alliance"] == "": futureMatches.append(i)
+    print(f"-- {futureMatches[0]}\n")
     if len(futureMatches) > 0: futureMatches = format(futureMatches)
+    print(f"--- {futureMatches[0]}\n")
     # print(futureMatches[0], "\n")
     data = {
         "all": allMatches,
