084d1e30465d3654ed438823eb9fbc1e68f6a93a
YayBlaze <blazeannison@icloud.com>
2025-03-07 14:23:31 -0800

    schedule update

--- tbaPulseData.py
+++ tbaPulseData.py
@@ -74,11 +74,14 @@ def format(list):
         for i in range(len(blue)):
             blue[i] = blue[i][3:]
             if my_team_key in blue[i]: blue[i] = f"<strong><u style='color: #89CFF0;'>{blue[i]}</u></strong>" #highlight your teamkey
+            if "1844" in blue[i]: blue[i] = f"<strong><u>{blue[i]}</u></strong>"
             # if blue[i] in "".join(myNextMatch()["alliances"][myAlliance(myNextMatch())]["team_keys"]):
                 # blue[i] = f"<strong><u>{blue[i]}</u></strong>"
         for i in range(len(red)): 
             red[i] = red[i][3:]
             if my_team_key in red[i]: red[i] = f"<strong><u style='color: #EE4B2B;'>{red[i]}</u></strong>"
+            if "1844" in red[i]: red[i] = f"<strong><u>{red[i]}</u></strong>"
+
             # if red[i] in "".join(myNextMatch()["alliances"][myAlliance(myNextMatch())]["team_keys"]): 
                 # red[i] = f"<strong><u>{red[i]}</u></strong>"
 
