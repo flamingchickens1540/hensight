2417abab5e6a495cb8dbb5e6e033b5b37636508e
YayBlaze <blazeannison@icloud.com>
2025-03-08 08:41:02 -0800

    i did a silly

--- TBAData.py
+++ TBAData.py
@@ -8,7 +8,7 @@ api_key: Final[str] = os.getenv("tba")
 tba = tbapy.TBA(api_key)
 thisYear = os.getenv("year")
 
-async def load_events(year, genKeys=False, write=False, shouldProgress=False):
+def load_events(year, genKeys=False, write=False, shouldProgress=False):
     keys = []
     data = {}
     print('-- Getting Match Keys')
