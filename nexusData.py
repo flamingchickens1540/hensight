3b5d13913f99910ff03df0fd3134cc5506da9209
YayBlaze <blazeannison@icloud.com>
2025-04-05 11:15:01 -0700

    fixed an error

--- nexusData.py
+++ nexusData.py
@@ -213,7 +213,10 @@ def getNexusData():
             
         #tasks
         # pulseData["tasks"] = genTasks()
-        pulseData["tasks"] = f"<p><strong>Now Queueing: </strong>{data['nowQueuing']}</p><p><strong>On Deck: </strong>{next(filter(lambda m: m['status'] == 'On deck', data['matches']), 'None')['label']}</p><p><strong>On Field: </strong>{list(filter(lambda m: m['status'] == 'On field', data['matches']))[-1]['label']}</p>"
+        try:
+            nowQueue = data['nowQueuing']
+        except KeyError: nowQueue = "None"
+        pulseData["tasks"] = f"<p><strong>Now Queueing: </strong>{nowQueue}</p><p><strong>On Deck: </strong>{next(filter(lambda m: m['status'] == 'On deck', data['matches']), 'None')['label']}</p><p><strong>On Field: </strong>{list(filter(lambda m: m['status'] == 'On field', data['matches']))[-1]['label']}</p>"
 
       
     return pulseData
--- tbaPulseData.py
+++ tbaPulseData.py
@@ -25,7 +25,7 @@ def getRankings():
         i["team_key"] = i["team_key"][3:]
         if my_team_key in str(i["team_key"]):
             i["team_key"] = f'<strong style="color: #f6b14b;">{i["team_key"]}</strong>'
-        postFormat.append(f'<p style="font-size: 2rem; line-height:0; height:fit-content;">{str(i["rank"])}) {str(i["team_key"])}</p>')
+        postFormat.append(f'<p style="font-size: 2rem; line-height:0; height:fit-content; margin:1%; border:0;">{str(i["rank"])}) {str(i["team_key"])}</p>')
         # postFormat.append(f"<div class=\"schedulelement\"><p'>{str(i['rank'])}) </p><p>{str(i['team_key'])}</p></div>")
     # print(top10)
     return postFormat
