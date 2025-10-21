f76180d5ea56c14ebffcbafe3f45ba08a86704ee
YayBlaze <blazeannison@icloud.com>
2025-10-21 14:38:59 -0700

    feat: built out timer

--- frontend/src/routes/timer.svelte
+++ frontend/src/routes/timer.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+    let match = "QM48"
+    let time = "8:32"
+</script>
+
 <div class="border-4 border-(--color-white) rounded-lg size-full">
-    <h1>Timer</h1>
+    <h1 class="text-[4rem] p-1 text-(--color-blue)">{match}</h1>
+    <div class="size-full flex justify-center align-center"><h1 class="text-[10rem] text-(--color-green) font-extrabold">{time}</h1></div>
 </div>
