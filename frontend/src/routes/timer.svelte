3e04455a09d22da101b73dffdd374be2a47c81c7
YayBlaze <blazeannison@icloud.com>
2025-10-22 13:41:09 -0700

    fix: color wasn't working with timer

--- frontend/src/routes/timer.svelte
+++ frontend/src/routes/timer.svelte
@@ -2,10 +2,9 @@
     let match = "QM48"
     let time = "15:40"
     let currentTime = "18:44:00"
-    let color = "--color-green"
 </script>
 
 <div class="border-4 border-(--color-white) rounded-lg size-full">
     <div class="flex justify-between items-center"><h1 class="text-[4rem] p-1 text-(--color-blue)">{match}</h1><h1 class="text-[3rem] p-1">{currentTime}</h1></div>
-    <div class="size-full flex justify-center"><h1 class="text-[10rem] text-({color}) font-extrabold">{time}</h1></div>
+    <div class="size-full flex justify-center"><h1 class="text-[10rem] text-(--color-green) font-extrabold">{time}</h1></div>
 </div>
