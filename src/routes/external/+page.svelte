f66ee4ee915a7ab4b29d48b3b5a557689ac47c02
Blaze Annison <blaze@Blazes-MacBook-Pro.local>
2026-01-15 13:37:07 -0800

    feat: added favicon and external nav

---
+++ src/routes/external/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { goto } from '$app/navigation';
+</script>
+
+<div class="align- m-auto flex h-67.5 w-full items-center justify-around text-7xl">
+	<button
+		class="rounded-xl border-10 border-[#ffb347] p-1"
+		onclick={() => {
+			goto('/internal/stats');
+		}}>Statistics</button
+	>
+</div>
