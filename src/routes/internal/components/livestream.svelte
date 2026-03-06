7fed9aea15952cf896767a689a933a1834c26c54
Blaze Annison <blaze@Blazes-MacBook-Pro.local>
2026-03-06 09:49:19 -0800

    fix: fixed stream and schedule (temp fix)

--- src/routes/internal/components/livestream.svelte
+++ src/routes/internal/components/livestream.svelte
@@ -3,22 +3,21 @@
 </script>
 
 <div class="border-white size-full rounded-lg border-4">
-	<iframe
+	<!-- <iframe
         title="twitchstream"
 		src="https://player.twitch.tv/?channel={channel}&parent=hensight.yayblaze.com"
 		height="100%"
 		width="100%"
 	>
-	</iframe>
-	<!-- <iframe 
-		width="100%" 
-		height="100%" 
-		src="https://www.youtube-nocookie.com/embed/9FfipFHWutE?si=jMtmRFNUKdCU1vhX&amp;controls=0" 
-		title="YouTube video player" 
-		frameborder="0" 
-		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-		referrerpolicy="strict-origin-when-cross-origin" 
-		allowfullscreen
-	>
 	</iframe> -->
+	<iframe 
+	width="100%" 
+	height="100%" 
+	src="https://www.youtube-nocookie.com/embed/x-4VAVyb4as?amp;controls=0" 
+	title="YouTube video player" 
+	frameborder="0" 
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+	referrerpolicy="strict-origin-when-cross-origin" 
+	allowfullscreen>
+	</iframe>
 </div>
