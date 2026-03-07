4a40e0fa18c4484130b9c0157e8aa50dff895f28
Blaze Annison <blaze@Blazes-MacBook-Pro.local>
2026-03-07 08:42:43 -0800

    fix: stream actaully works now

--- src/routes/internal/components/livestream.svelte
+++ src/routes/internal/components/livestream.svelte
@@ -10,14 +10,5 @@
 		width="100%"
 	>
 	</iframe> -->
-	<iframe 
-	width="100%" 
-	height="100%" 
-	src="ttps://www.youtube-nocookie.com/embed/h-qcbsZw-xs" 
-	title="YouTube video player" 
-	frameborder="0" 
-	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-	referrerpolicy="strict-origin-when-cross-origin" 
-	allowfullscreen>
-	</iframe>
+	<iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/h-qcbsZw-xs?si=OWERRtvUUebGfKPP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </div>
