00fccc5472ce66d921158b497c13fc585ff89820
YayBlaze <blazeannison@icloud.com>
2026-03-13 22:18:05 -0700

    fix: removed outreach button until i have content

--- src/routes/external/+page.svelte
+++ src/routes/external/+page.svelte
@@ -15,12 +15,12 @@
 			goto('/external/cad');
 		}}>CAD</button
 	>
-	<button
+	<!-- <button
 		class="rounded-xl border-10 border-[#ffb347] p-1"
 		onclick={() => {
 			goto('/external/outreach');
 		}}>Outreach</button
-	>
+	> -->
 	<button
 		class="rounded-xl border-10 border-[#800000] p-1"
 		onclick={() => {
