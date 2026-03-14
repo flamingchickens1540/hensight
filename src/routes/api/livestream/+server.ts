1a3b786725816933f105be0b0de1b56ee0645a00
YayBlaze <blazeannison@icloud.com>
2026-03-13 22:00:55 -0700

    forgot to add the file í²

---
+++ src/routes/api/livestream/+server.ts
@@ -0,0 +1,18 @@
+import { eventKey } from '$lib/config';
+import { getStreamID } from '$lib/tba';
+import { json, type RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async () => {
+	let data = {
+        hasData: false,
+        channelID: ''
+    }
+
+    const res = await getStreamID(eventKey)
+    if (res) {
+        data.hasData = true
+        data.channelID = res
+    }
+
+	return json(data);
+};
