49d20383ff63d61f235bb9f85aa597954f2de50c
YayBlaze <blazeannison@icloud.com>
2026-04-10 18:14:19 -0700

    fix: stream now uses local date instead of UTC

--- src/lib/tba.ts
+++ src/lib/tba.ts
@@ -43,7 +43,7 @@ export async function getStreamID(eventKey: string) {
 	return (
 		webcasts.find(
 			(stream) =>
-				new Date(stream.date).getUTCDate() == new Date().getUTCDate() || stream.type == 'twitch'
+				new Date(stream.date).getUTCDate() == new Date().getDate() || stream.type == 'twitch'
 		) ?? webcasts[0]
 	);
 }
