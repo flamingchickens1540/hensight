71ae06f5bc221218e63e3034c27d7ad8525da330
YayBlaze <blazeannison@icloud.com>
2026-09-20 11:01:07 -0700

    fix: fixed dev port

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,14 @@
 module.exports = {
-  apps: [
-    {
-      name: "hensight",
-      script: "bun",
-      args: "build/index.js",
-      interpreter: "none",
-      env: {
-        PORT: 5300,
-        HOST: "0.0.0.0"
-      }
-    }
-  ]
+	apps: [
+		{
+			name: 'hensight',
+			script: 'bun',
+			args: 'build/index.js',
+			interpreter: 'none',
+			env: {
+				PORT: 5302,
+				HOST: '0.0.0.0'
+			}
+		}
+	]
 };
